d7273c8530f76ba4c6246e58be5a4fffa37b52c8
Lucas Farias <135982748+devlucascfarias@users.noreply.github.com>
2024-06-10 18:37:51 -0300

    Update gpt2.py

--- gpt2.py
+++ gpt2.py
@@ -2,23 +2,21 @@ import streamlit as st
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import time
 
-# Load the tokenizer and model
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 model = GPT2LMHeadModel.from_pretrained("gpt2")
 name_model = model.name_or_path
 
-# Page title
-st.title('GPT-2 Model')
+st.set_page_config(page_title='GPT-2 Model', page_icon=':robot:')
 
-# Sidebar for input parameters
+st.title('GPT-2 Model')
+st.write('This is a simple web app to interact with the GPT-2 model. The objective of the project is to run GPT technology locally on less powerful computers. The GPT-2 model is a text completer, that is, it is not good for answering your questions, but rather for developing texts based on an existing text. Note that the GPT -2 model has fewer parameters, so it responds less concisely.')
+
 with st.sidebar:
     st.header('Model Parameters')
-    temperature = st.slider('Temperature', min_value=0.1, max_value=1.0, value=0.7, step=0.1)
-    max_length = st.slider('Max Length', min_value=50, max_value=700, value=200, step=50)
+    temperature = st.slider('Temperature', min_value=0.1, max_value=1.0, value=0.7, step=0.01)
+    max_length = st.slider('Max Length', min_value=50, max_value=700, value=200, step=10)
 
 
-
-# Main content
 input_text = st.text_input('Type here')
 logs = []
 
